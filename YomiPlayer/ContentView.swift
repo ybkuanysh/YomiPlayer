e9e996af5bdfb2d309c57828ae8edba9d184bc71
Kuanysh Yabekov <kuanyshyabekov.dev@gmail.com>
2025-07-24 19:00:50 +0500

    Initial Commit

---
+++ YomiPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YomiPlayer
+//
+//  Created by Kuanysh Yabekov on 24.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YomiPlayer/YomiPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YomiPlayerApp.swift
+//  YomiPlayer
+//
+//  Created by Kuanysh Yabekov on 24.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct YomiPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
